30114406a23f3b02e94d69b31747edfaacbd99aa
XIXIL <ya.mr-adam@yandex.com>
2023-04-12 23:00:42 +0500

    bug

---
+++ src/Fraction.java
@@ -0,0 +1,130 @@
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        if (numerator != 0 && denominator != 0) {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    public Fraction addition(Fraction fraction) {
+        if (fraction.numerator == 0 || fraction.denominator == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int newDenominator = leastCommonMultiple(fraction.denominator, this.denominator);
+        int newNumerator = fraction.numerator * (newDenominator / fraction.denominator) +
+                this.numerator * (newDenominator / this.denominator);
+
+        return simplify(newNumerator, newDenominator);
+    }
+
+    public Fraction addition(int fractionNumerator, int fractionDenominator) {
+        if (fractionNumerator == 0 || fractionDenominator == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int newDenominator = leastCommonMultiple(fractionDenominator, this.denominator);
+        int newNumerator = fractionNumerator * (newDenominator / fractionDenominator) +
+                this.numerator * (newDenominator / this.denominator);
+
+        return simplify(newNumerator, newDenominator);
+    }
+
+    public Fraction subtraction(Fraction fraction) {
+        //TODO
+        if (fraction.numerator == 0 || fraction.denominator == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int newDenominator = leastCommonMultiple(fraction.denominator, this.denominator);
+        int newNumerator = this.numerator * (newDenominator / this.denominator)
+                - fraction.numerator * (newDenominator / fraction.denominator);
+
+        return simplify(newNumerator, newDenominator);
+    }
+
+    public Fraction subtraction(int fractionNumerator, int fractionDenominator) {
+        //TODO
+        if (fractionNumerator == 0 || fractionDenominator == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int newDenominator = leastCommonMultiple(fractionDenominator, this.denominator);
+        int newNumerator = this.numerator * (newDenominator / this.denominator)
+                - fractionNumerator * (newDenominator / fractionDenominator);
+
+        return simplify(newNumerator, newDenominator);
+    }
+
+    public Fraction multiplication(int fractionNumerator, int fractionDenominator) {
+        if (fractionNumerator == 0 || fractionDenominator == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return simplify(this.numerator * fractionNumerator, this.denominator * fractionDenominator);
+    }
+
+    public Fraction multiplication(Fraction fraction) {
+        if (fraction.numerator == 0 || fraction.denominator == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return simplify(this.numerator * fraction.numerator, this.denominator * fraction.denominator);
+    }
+
+    public Fraction division(int fractionNumerator, int fractionDenominator) {
+        if (fractionNumerator == 0 || fractionDenominator == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return simplify(this.numerator * fractionDenominator, this.denominator * fractionNumerator);
+    }
+
+    public Fraction division(Fraction fraction) {
+        if (fraction.numerator == 0 || fraction.denominator == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return simplify(this.numerator * fraction.denominator, this.denominator * fraction.numerator);
+    }
+
+    private Fraction simplify(int numerator, int denominator) {
+        int newNumerator = leastCommonMultiple(denominator, numerator) / Math.max(numerator, denominator);
+
+        if (newNumerator < numerator) {
+            return new Fraction(newNumerator, (denominator / (numerator / newNumerator)));
+        }
+
+        return new Fraction(numerator, denominator);
+    }
+
+    private static int greatestCommonDivisor(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return greatestCommonDivisor(b, a % b);
+    }
+
+    private static int leastCommonMultiple(int a, int b) {
+        return a * b / greatestCommonDivisor(a, b);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        Fraction fraction1 = new Fraction(12, 345);
+        Fraction fraction2 = new Fraction(1, 4);
+        Fraction fraction3 = new Fraction(5, 15);
+        Fraction fraction4 = new Fraction(24, 80);
+
+//        System.out.println(fraction1.addition(fraction2)); //true
+//        System.out.println(fraction1.addition(fraction3)); //true
+//        System.out.println(fraction1.addition(fraction4)); //true
+
+//        System.out.println();
+
+        System.out.println(fraction1.subtraction(fraction2)); //false
+        System.out.println(fraction1.subtraction(fraction3)); //true
+        System.out.println(fraction1.subtraction(fraction4)); //false
+
+//        System.out.println();
+
+//        System.out.println(fraction1.multiplication(fraction2)); //true
+//        System.out.println(fraction1.multiplication(fraction3)); //true
+//        System.out.println(fraction1.multiplication(fraction4)); //true
+
+//        System.out.println();
+//
+//        System.out.println(fraction1.division(fraction2)); //true
+//        System.out.println(fraction1.division(fraction3)); //true
+//        System.out.println(fraction1.division(fraction4)); //true
+
+    }
+}
